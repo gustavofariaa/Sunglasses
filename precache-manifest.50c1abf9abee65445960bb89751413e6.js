1e3c6947a6b9f52ac889e4b5c6f6a9faea1da91f
Gustavo <gufariaa@gmail.com>
2020-08-19 07:21:44 -0300

    Updates

---
+++ precache-manifest.50c1abf9abee65445960bb89751413e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3067133549f32b7893366181a9acf96",
+    "url": "/Sunglasses/index.html"
+  },
+  {
+    "revision": "14c7ac50c4895c388b30",
+    "url": "/Sunglasses/static/js/2.14aa9239.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Sunglasses/static/js/2.14aa9239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a6cd66f0043748818e",
+    "url": "/Sunglasses/static/js/main.5e3f7feb.chunk.js"
+  },
+  {
+    "revision": "8ff6d7813c27d95e3ffe",
+    "url": "/Sunglasses/static/js/runtime-main.ec417bf9.js"
+  }
+]);
